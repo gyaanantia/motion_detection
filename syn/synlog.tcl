bf71f3126de8a62d450943325063903383690725
gyaanantia <>
2023-02-06 02:40:34 -0600

    done

--- syn/synlog.tcl
+++ syn/synlog.tcl
@@ -2,11 +2,7 @@ history clear
 add_file -verilog /home/gfa2226/fpga/hw3/motion_detect/fifo.sv
 add_file -verilog /home/gfa2226/fpga/hw3/motion_detect/grayscale.sv
 add_file -verilog /home/gfa2226/fpga/hw3/motion_detect/highlight.sv
-add_file -verilog /home/gfa2226/fpga/hw3/motion_detect/motion_detect_tb.sv
 add_file -verilog /home/gfa2226/fpga/hw3/motion_detect/motion_detect_top.sv
 add_file -verilog /home/gfa2226/fpga/hw3/motion_detect/subtract.sv
-project -run  
-project -run  
-project_file -remove /home/gfa2226/fpga/hw3/motion_detect/motion_detect_tb.sv
 project -run  
 project -close /home/gfa2226/fpga/hw3/motion_detect/syn/proj_1.prj
